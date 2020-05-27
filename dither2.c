57aabdabd84a03c635ed95427f4fda5e82d74103
wenhungyang <wenhung.yang@linfiny.com>
2020-05-27 14:33:25 +0800

    using int_8, divid error by multiple of 2

--- dither2.c
+++ dither2.c
@@ -7,8 +7,9 @@ void before()
 void rgbaToBw(uint32_t *data, int width, int height, long stride)
 {
     int32_t row, col;
-    uint8_t *r, *g, *b, q_err, q_error_right, *q_error_array, *q_error;
+    uint8_t *r, *g, *b;
     uint32_t bw, q_bw, pixel_index;
+    int8_t q_err, q_error_right, *q_error_array, *q_error;
 
     q_error_array = calloc(1, width + 2);
     q_error = q_error_array + 1;
@@ -19,16 +20,25 @@ void rgbaToBw(uint32_t *data, int width, int height, long stride)
             g = (uint8_t *) data + 1;
             b = (uint8_t *) data;
             bw = (*r * 0.299) + (*g * 0.587) + (*b * 0.114);
-            bw += q_error[col];
-	    q_bw = bw & 0x80 ? 0xff : 0x00;
-	    //q_bw = bw & 0xf0;
-	    q_err = bw - q_bw;
-	    q_error[col - 1] = (q_err * 3) >> 4;
+	    if(row != 0)
+	            bw += q_error[col];
+	    if(bw >= 0xf0) {
+		q_bw = 0xf0;
+		q_err = 0;
+	    } else if(bw <= 0) {
+		q_bw = 0x0;
+		q_err = 0;
+	    } else {
+	    	q_bw = bw & 0x80 ? 0xf0 : 0x00;
+	    	q_err = bw - q_bw;
+	    }
+
+	    q_error[col - 1] = q_err >> 3;
 	    if(col == 0)
-		    q_error[col] = (q_err * 8) >> 4;
+		    q_error[col] = q_err >> 1;
 	    else
-		    q_error[col] = (q_err * 5) >> 4;
-	    q_error[col + 1] = q_err * 8 >> 4;
+		    q_error[col] = q_err >> 2;
+	    q_error[col + 1] = q_err >> 1;
 
             *r = *g = *b = q_bw;
 
